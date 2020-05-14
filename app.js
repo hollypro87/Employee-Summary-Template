e48a4549733607cb103fbbdbe333262babd66a88
Holly Prothe <hollyprothe1@gmail.com>
2020-05-14 16:36:36 -0500

    question code written. issues with rendering document. needs resolved.

--- app.js
+++ app.js
@@ -10,26 +10,128 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+async function askManager() {
+    return await inquirer
+        .prompt([{
+            type: "input",
+            name: "name",
+            message: "What is your manager's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your manager's id?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your manager's email?",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your manager's office number?",
+        }
+        ]);
+}
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+async function askEngineer() {
+    return await inquirer
+        .prompt([{
+            type: "input",
+            name: "name",
+            message: "What is your engineer's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your engineer's id?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your engineer's email?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your engineer's Github username?",
+        }
+        ]);
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+async function askIntern() {
+    return await inquirer
+        .prompt([{
+            type: "input",
+            name: "name",
+            message: "What is your intern's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your intern's id?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your intern's email?",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is your intern's school?",
+        }
+        ])
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+async function addEmployee() {
+    return await inquirer
+        .prompt([{
+            type: "list",
+            name: "role",
+            message: "Which type of employee would you like to add?",
+            choices: [{
+                name: "Engineer",
+                value: "eng"
+            },
+            {
+                name: "Intern",
+                value: "int"
+            },
+            {
+                name: "My Team is now complete!",
+                value: "none"
+            }
+            ]
+        }]);
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+async function init() {
+    const employees = [];
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    const managerRes = await askManager();
+    const manager = new Manager(managerRes.name, managerRes.id, managerRes.email, managerRes.officeNumber);
+    employees.push(manager);
+
+    let employeeRole = await addEmployee();
+
+    while (employeeRole.role !== "none") {
+
+        if (employeeRole.role === "eng") {
+            const engineerRes = await askEngineer();
+            const engineer = new Engineer(engineerRes.name, engineerRes.id, engineerRes.email, engineerRes.github);
+            employees.push(engineer);
+        } else {
+            const internRes = await askIntern();
+            const intern = new Intern(internRes.name, internRes.id, internRes.email, internRes.school);
+            employees.push(intern);
+        }
+
+        employeeRole = await addEmployee();
+    }
+    const outputHTML = render(employees);
+    await fs.writeFile(outputPath, outputHTML)
+}
+
+init();
